67bf544c78bb229109e951c4e6f0ab768b66fd53
kurehajime <xiidec@gmail.com>
2021-10-25 13:29:47 +0000

    upgrade design

--- app/javascript/packs/make/index.js
+++ app/javascript/packs/make/index.js
@@ -17,7 +17,14 @@ function makeURL(e) {
     document.querySelector('#result').value = url
     document.querySelector('#result_tag').value = `![sendokun](${url})`
 }
+function copyText(id) {
+    const copyTarget = document.getElementById(id);
+    copyTarget.select();
+    navigator.clipboard.writeText(copyTarget.value)
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#make').addEventListener('click', makeURL)
+    document.querySelector('#copy_result').addEventListener('click', () => { copyText("result") })
+    document.querySelector('#copy_result_tag').addEventListener('click', () => { copyText("result_tag") })
 });
